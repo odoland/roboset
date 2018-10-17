7d8018645373397ad8d525fe0f2d0769d2d63828
odoland <orlando.wong@yahoo.com>
2018-10-16 18:05:18 -0700

    Code cleanups + Algorithm efficiency improvements
    Switch to a hashing for set matching, allowed for refreshing screenshot.
    Added tint handling features - for grey and green. TODO: red for errors - will need unclicking or a longer sleep to slow down.

---
+++ CardGen.py
@@ -0,0 +1,68 @@
+# Test if this produces the correct card ID
+
+import numpy as np
+import cv2
+
+from SetCard import *
+from ColorDetect import *
+from FillDetect import *
+from ShapeDetect import *
+
+KERNEL = np.ones((5,5), np.uint8) # Kernel for the erosion and dilation (for solid /hollow only)
+PURPB = np.array ([80, 0, 80]), np.array([160,60,160])
+GREENB = np.array([0,100,0]), np.array([45,180,45])
+REDB = np.array([0,0,160]), np.array([30,30,255])
+
+
+
+def create_Card(img, kernel, debug=False):
+	""" 
+	Pass in the path to the image file and a kernel - will generate the card
+	"""
+	image = cv2.imread(img) # colored 
+	image0 = cv2.imread(img,0) # grayscale
+
+	# Color detection
+	color, pixel_count = find_Color(image, PURPB, GREENB, REDB)
+
+	# Fill detection
+	if is_Stripe(image0,12):
+		fill = STRIPE
+	else:
+		fill = Hollow_or_Full(image0, KERNEL,color,pixel_count)
+
+	if fill == HOLLOW: # The solid green oval shapes may be mistakenly taken as hollow
+		processed_img = processHollow(image0)
+	else: 
+		processed_img = processStripesFull(image0, KERNEL)
+ 	
+ 	# Shape & count detection
+	shape, count = detectShapeCount(processed_img)
+	
+	if debug:
+		print("creating a card:")
+		print(f"shape{shape}")
+		print(f"color{color}")
+		print(f"fill{fill}")
+		print(f"count{count}")
+
+	return SetCard(shape,color,fill,count)
+
+if __name__ == '__main__':
+	import argparse
+
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-i", "--image", required=True, help="Path to the set card img")
+	ap.add_argument("-d", "--debug", action="store_true") # flag for debug
+	args = vars(ap.parse_args())
+
+	image = args['image']
+
+	# image = image [ 4 :-4]
+	# image0 = image0 [ 4 :-4] (optional cropping)
+
+	debug = args['debug']
+
+	s_card = create_Card(image,KERNEL,debug) # call function in debug mode
+
+
--- FillDetect.py
+++ FillDetect.py
@@ -1,61 +1,71 @@
 # Functions to figure out the fill
 import cv2
 import numpy as np 
-
-from scipy.signal import argrelextrema
 from scipy.signal import find_peaks
-import peakutils
 
-from SetCard import HOLLOW, STRIPE, FULL
+from SetCard import HOLLOW, STRIPE, FULL, GREEN, RED, PURPLE
+from ColorDetect import find_Color
+
+# Arbitrary peak threshold value (as percentage of the maximum peak length)
+PEAK_THRESH = 0.33 # my default is set at approx 1/3rd
 
 
-# First process the stripe:
 def is_Stripe(image, number_peaks=12):
-	""" Returns boolean, if striped. Parameters: threshold for peaks """
+	""" Returns boolean, if striped. Parameters: threshold for the count of peaks to be qualified as a stripe.
+	What work well so far as of 10/ """
 
-	# First we take the derivative (f'(x)) on the y-axis to see if there are crazy changes
-	fpx = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=5)
-	vpr= cv2.reduce(fpx, 1, cv2.REDUCE_SUM)
-	vpr_p= np.sqrt(vpr**2).flatten()
+	# Crop the edges
+	image = image [ 4 :-4]
 
-	# Each change or "stripe" is essentially a peak on the f'(x). 
-	unfiltered_peaks, _ = find_peaks(vpr_p, height=0)
+	# Seek crazy changing  rates across the y-axis of the image w/ Sobel algorithm to estimate the first derivative
+	fpx = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=5) # Take only the across y-axis (vertically)
+	vpr= cv2.reduce(fpx, 1, cv2.REDUCE_SUM) # Project it onto a 1D np array
+	vpr_p= np.sqrt(vpr**2).flatten() # Flip the signs of negative changes
+
+	# Large spikes on f'(x) indicate change in the rate of change - seek areas where slope of the second derivative is 0
+	unfiltered_peaks, _ = find_peaks(vpr_p, height=0) # Finding peaks
 	all_peaks = (vpr_p[i] for i in unfiltered_peaks)
-	peak_thresh = max(all_peaks) * 0.33 # arbitrary weight 1/3 of the max_peak
-	
+	peak_thresh = max(all_peaks) * PEAK_THRESH 
 	filtered_peaks = sum(1 for i in unfiltered_peaks if vpr_p[i] >= peak_thresh)
 	
 	return (filtered_peaks) > number_peaks
 
-def Hollow_or_Full(image,kernel):
-	""" Pass in an erosion/dilation kernel. Image must be cv2.imread(image,0)  """
 
-	# erode/dilate to magnify the thin pixels
+def Hollow_or_Full(image, kernel, color, pixel_count):
+	""" Pass in an erosion/dilation kernel. Image must be cv2.imread(image,0). Pass in the color and pixel_count reads
+	from ColorDetect module.  """
+
+	# Image preprocessing - magnification of the thinly drawn hollow. TODO: solids should be exempt
 	erosion = cv2.erode(image,kernel,iterations=1)		
 	dilation = cv2.dilate(erosion,kernel,iterations=1)
-	_, thresh = cv2.threshold(dilation,200,255,1)
+	_, thresh = cv2.threshold(dilation,200,255,1) # Threshold cut off value at 200 (white)
 
 	# mark the change from white to black
 	first_deriv_y = cv2.Sobel(thresh, cv2.CV_64F, 0,1,ksize=5)
-	vpr_1 = cv2.reduce(first_deriv_y, 1, cv2.REDUCE_SUM)
-	vpr_1_p = np.sqrt(vpr_1**2).flatten()
+	vpr_1y = cv2.reduce(first_deriv_y, 1, cv2.REDUCE_SUM)
+	vpr_1y_p = np.sqrt(vpr_1y**2).flatten()
 
 	# Find every peak index, and access the height of the peak
-	unfiltered_peaks, _ = find_peaks(vpr_1_p, height=0)
-	all_peaks = (vpr_1_p[i] for i in unfiltered_peaks)
+	unfiltered_peaks_y, _ = find_peaks(vpr_1y_p, height=0)
+	all_peaks_y = (vpr_1y_p[i] for i in unfiltered_peaks_y)
 	
 	# I just arbitrarily set the threshold to .32 (noise reduction)
-	peak_thresh = max(all_peaks) * .32 # cuto ff is 1/3rd of the highest peak
+	peak_thresh_y = max(all_peaks_y) * .32 # cuto ff is 1/3rd of the highest peak
+	approx_peaks_y= sum(1 for i in unfiltered_peaks_y if vpr_1y_p[i] >= peak_thresh_y)
 
 
-	approx_peaks = sum(1 for i in unfiltered_peaks if vpr_1_p[i] >= peak_thresh)
-
-	if approx_peaks == 6: # is also a diamond
+	# Todo: Be replaced with an actual neural network
+	if approx_peaks_y == 6: # Is also a diamond # To do, add extra weight to that
 		return HOLLOW 
-	elif approx_peaks == 2:
+	elif approx_peaks_y == 2:
 		return FULL
-	elif approx_peaks == 4:
-		return HOLLOW 
+	elif approx_peaks_y == 4: # Unique case . can be either hollow or a solid oval
+		if color == GREEN and pixel_count > 3500: # Density of green cards approx, >3500 - PARAMETER
+			return FULL
+		else:
+			return HOLLOW 
+	else:
+		raise TypeError("Cannot find fill type. Possibly invalid picture. ")
 
 
 
@@ -63,21 +73,35 @@ if __name__ == '__main__':
 	import argparse
 	from matplotlib import pyplot as plt 
 
+	print("You've entered debugger mode :)")
+
 	ap = argparse.ArgumentParser()
 	ap.add_argument("-i", "--image", required=True, help="path to the input image")
 	ap.add_argument("-d", "--debug", action="store_true")
+	ap.add_argument("-g", "--green_oval", action="store_true", help="is a solid green oval")
+	
 	args = vars(ap.parse_args())
 
 	debug = args['debug']
+
+	PURPB = np.array ([80, 0, 80]), np.array([160,60,160])
+	GREENB = np.array([0,100,0]), np.array([45,180,45])
+	REDB = np.array([0,0,160]), np.array([30,30,255])
+
+	color_image = cv2.imread(args['image'])
 	image = cv2.imread(args['image'],0) # read image in BW 
 	image = image [ 4 :-4] # Numpy slice the last 4 pixels (noise removal)
+	# cv2.imshow("sliced", image)
+	# cv2.waitKey(0)
 
 	kernel = np.ones((5,5), np.uint8) # Kernel for the erosion and dilation
 
+	color, pixel_count = find_Color(color_image,PURPB,GREENB,REDB)
+	
 	if is_Stripe(image,12): # using 12 
 		print("is a stripe")
 	else:
-		fill = Hollow_or_Full(image, kernel)
+		fill = Hollow_or_Full(image, kernel,color, pixel_count)
 		if fill == FULL:
 			print("is full")
 		elif fill == HOLLOW:
